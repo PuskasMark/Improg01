359683c3defc454a9954cc10887ea1276da73179
Puskás Márk <113339392+PuskasMark@users.noreply.github.com>
2022-09-12 10:22:43 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int n=42;
+    unsigned int un=10; //pozitív szám
+    float f=2.25;
+    double d =2.2545215;
+    char c = 'a';
+
+   printf("Itt vannak a számok he:%d %u %f %f %c\n",n,un,f,d,c);
+    
+}
